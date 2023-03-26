3b3361b1bb25e469f7d12622d36e48ebcfe3d70e
marlisonmourao <bentesmourao@gmail.com>
2023-03-26 06:22:53 -0400

    configuring application global header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
+import logoImg from '../assets/logo.svg'
+
+import { Container, Header } from '../styles/pages/app'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      teste
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
       <Component {...pageProps} />
-    </div>
+    </Container>
   )
 }
